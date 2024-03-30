bafdb124fa1a1ec5facb3182d7ae68fbc1ffc8a2
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-03-30 12:29:26 +0530

    Create 3 Chef_&_InstantNoodles.cpp
    Chef has invented 1-minute Instant Noodles. As the name suggests, each packet takes exactly 1 minute to cook.
    
    Chef's restaurant has X stoves and only 1 packet can be cooked in a single stove at any minute.
    
    How many customers can Chef serve in Y minutes if each customer orders exactly 1 packet of noodles?

---
+++ 3 Chef_&_InstantNoodles.cpp
@@ -0,0 +1,15 @@
+//multiplication
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int X,Y;
+	cin >>X>>Y;
+	
+	int total_packets = X*Y;
+	cout << total_packets << endl;
+	return 0;
+
+}
