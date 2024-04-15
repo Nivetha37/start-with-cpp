ce8d4a549b0a9427139690e7768575307bd5e63a
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-04-15 22:19:36 +0530

    Create 0.4  TEST_CASES.cpp

---
+++ 0.4  TEST_CASES.cpp
@@ -0,0 +1,23 @@
+/*Lets solve a simple problem.
+Write a program in the IDE which does the following
+
+Accepts 5 inputs given on 5 separate lines. Each input is an integer N
+For each test cases, prints out the integer N to console on a separate line (our Input mirror problem)*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int A, B, C, D, E;
+    cin >> A;
+    cin >> B;
+    cin >> C;
+    cin >> D;
+    cin >> E;
+    cout << A << endl;
+    cout << B << endl;
+    cout << C << endl;
+    cout << D << endl;
+    cout << E <<endl;
+}
