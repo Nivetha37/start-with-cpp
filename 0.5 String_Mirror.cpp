faf75672fc4bfb8bbdf4e3d8aa6a1a2f95f260f4
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-04-16 22:21:02 +0530

    Create 0.5 String_Mirror.cpp

---
+++ 0.5 String_Mirror.cpp
@@ -0,0 +1,18 @@
+//Create a variable X which contains the string S concatenated with the string S
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+ int t;
+ cin>>t;
+ 
+while(t--)
+ { 
+   string S,X;
+   cin>>S;
+   X=S+S;
+   cout<<X<<endl;
+  }
+ 
+}
