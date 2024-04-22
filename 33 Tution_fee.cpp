4204aa1e35760e7c03edb3857106b61f4c3853f7
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-04-22 23:19:07 +0530

    Create 33 Tution_fee.cpp

---
+++ 33 Tution_fee.cpp
@@ -0,0 +1,16 @@
+/*You will attend tuitions for X weeks, and the cost of tuition per week is Y dollars.
+You need to compute and output your total tuition fees.*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t-- >0){
+        int x,y;
+        cin>>x>>y;
+        cout<<x*y<<endl;
+    }
+    return 0;
+}
