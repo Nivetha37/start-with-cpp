eea2c88050e4b0c26aa25575ea3dc11429048c54
Nivetha37 <124027942+Nivetha37@users.noreply.github.com>
2024-04-25 21:12:55 +0530

    Create 36 Fitness.cpp

---
+++ 36 Fitness.cpp
@@ -0,0 +1,16 @@
+/*Chef wants to become fit for which he decided to walk to the office and return home by walking.
+It is known that Chef's office is X km away from his home.
+If his office is open on 5 days in a week, find the number of kilometres Chef travels through office trips in a week.*/
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while(t-->0){
+        int x;
+        cin>>x;
+        cout<<x*2*5<<endl;
+    }
+}
